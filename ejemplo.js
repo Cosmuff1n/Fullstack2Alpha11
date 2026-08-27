f4d7971621536fc79a9a8a0edbcdcc5687f6be13
Charlie Cosumuffin <shirocinammon1@gmail.com>
2026-08-27 17:51:40 -0400

    Fixed Sum Frontend functions

--- ejemplo.js
+++ ejemplo.js
@@ -1,8 +1,9 @@
 //Declaración de variables
-var num1 = 3;
-var num2 = 4;
-//Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
+//var num1;
+//var num2;
+/*Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
 var suma = num1 + num2;
+*/
 //Se muestra el resultado en la consola
 console.log('La suma es: '+ suma)
 
@@ -27,6 +28,21 @@ var div = num1 / num2;
 //Se muestra el resultado en la consola
 console.log('La división es: '+ div)
 
+function sumar() {
+
+    const num1 = parseInt(document.getElementById('num1').value);
+
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    const suma = num1 + num2;
+
+    document.getElementById('result').value = suma;
+}
+/*
+function suma(num1,num2){
+return suma = num1 + num2;
+}*/
+
 //console.log("pojo")
 try{
 edad = 19
@@ -34,7 +50,6 @@ edad = 19
 catch{
     exception("error, debe ser numero entero")
 }
-
 
 if(edad > 0){
     if(edad < 18) console.log("Persona es menor de edad")
