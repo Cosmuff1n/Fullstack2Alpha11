d429dd3d6368481b297cc4684f8c0e7255ab14f6
Charlie Cosumuffin <shirocinammon1@gmail.com>
2026-08-27 17:18:02 -0400

    expandir calculadora

--- ejemplo.js
+++ ejemplo.js
@@ -6,8 +6,33 @@ var suma = num1 + num2;
 //Se muestra el resultado en la consola
 console.log('La suma es: '+ suma)
 
+//Declaración de variables
+var num1 = 3;
+var num2 = 4;
+//Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
+var resta = num1 - num2;
+//Se muestra el resultado en la consola
+console.log('La resta es: '+ resta)
+
+//Declaración de variables
+var num1 = 3;
+var num2 = 4;
+//Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
+var mult = num1 * num2;
+//Se muestra el resultado en la consola
+console.log('La multiplicación es: '+ mult)
+
+//Declaración de variables
+var num1 = 3;
+var num2 = 4;
+//Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
+var div = num1 / num2;
+//Se muestra el resultado en la consola
+console.log('La división es: '+ div)
+
+
 try{
-edad = 34
+edad = 19
 }
 catch{
     exception("error, debe ser numero entero")
@@ -28,3 +53,4 @@ if(edad > 0){
     }
 }
 else console.log("Ingrese un número positivo")
+
