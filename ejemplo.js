d17686b35a036f056554a7081a37f9657b286277
Charlie Cosumuffin <shirocinammon1@gmail.com>
2026-08-27 17:23:50 -0400

    Removed the redundant variables
    ctrl + H

--- ejemplo.js
+++ ejemplo.js
@@ -7,24 +7,21 @@ var suma = num1 + num2;
 console.log('La suma es: '+ suma)
 
 //Declaración de variables
-var num1 = 3;
-var num2 = 4;
+
 //Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
 var resta = num1 - num2;
 //Se muestra el resultado en la consola
 console.log('La resta es: '+ resta)
 
 //Declaración de variables
-var num1 = 3;
-var num2 = 4;
+
 //Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
 var mult = num1 * num2;
 //Se muestra el resultado en la consola
 console.log('La multiplicación es: '+ mult)
 
 //Declaración de variables
-var num1 = 3;
-var num2 = 4;
+
 //Se suman las variables num1 y num2 y el resultado se guarda en la variable suma
 var div = num1 / num2;
 //Se muestra el resultado en la consola
