ba4ca51d9db323882c2bbe27f0e806edf4487f78
Charlie Cosmuffin <145036409+Cosmuff1n@users.noreply.github.com>
2026-08-28 16:24:26 -0400

    termine la primera guia, falta la 2da

--- test.js
+++ test.js
@@ -1,37 +1,42 @@
 /*
-• Cree una segunda función que muestre los datos de Nombre y Apellidos
-y solicite dos montos (dos números enteros) para sueldo base actual y
-sueldo base promedio del semestre anterior, que ingrese el dato de si
-tiene cargas familiares autorizadas y, si tiene cargas familiares
-autorizadas, retorne otro entero que sea el valor que le corresponde
-según la tabla siguiente:
-• El valor para usar como renta es el monto del semestre anterior.
-• La salida será: “Al Trabajador NOMBRE APELLIDOS le corresponde
-valor de familiar (0, 3264, 10327 o 16828) por su renta del semestre
-anterior que es: monto (uno de los 4).
-• Cree una función que, para las personas que, si tienen asignación
-familiar, consulte la cantidad de “beneficiados con carga familiar (recibe
-como parámetro un número entero r que indique la cantidad de personas
-con el beneficio) y retorne el número que corresponda al monto a recibir
-por carga familiar, obtenido en el punto anterior, multiplicado por la
-cantidad de beneficiarios. Por ejemplo, una persona con beneficio de
-10327 y 3 cargas familiares deberá responder con 3098
+Cree un programa que utilice el objeto persona y entregue todos los datos obtenidos,
+de las funciones anteriores:
+La persona de:
+• Nombre:
+• Apellidos:
+• Cargas (si/no):
+• Cantidad de Cargas familiares:
+• Está en el tramo que corresponde al ingreso del semestre anterior:
+• Le corresponde por carga familiar el monto:
+• Le corresponde el monto total de carga familiar de:
+• Su sueldo del mes más las cargas familiares es de
 */
+
+
 
 nombre = "alvaro"
 apellido = "gonzalez"
 
 sb_actual = 10000
-sb_ant = 32000
+sb_ant = 439899
 
 cfamiliares = true;
 cantcargas = 3;
 
-function calcularValorFamiliar(sb_ant)
+function assignTramo(sb_ant)
   {
-    if(sb_ant < 429899) return 0;
-    else if(sb_ant >= 429899 && sb_ant < 627913) return 3264;
-  else if(sb_ant >= 627913 && sb_ant < 979330) return 10327;
+    if(sb_ant < 429899) return "a";
+    else if(sb_ant >= 429899 && sb_ant < 627913) return "b";
+  else if(sb_ant >= 627913 && sb_ant < 979330) return "c";
+  else return "d";
+  }
+
+  function calcularValorFamiliar(result_sb_ant)
+  //function assignTramo(result_sb_ant)
+  {
+    if(result_sb_ant=="a") return 0;
+    else if(result_sb_ant=="b") return 3264;
+  else if(result_sb_ant=="c") return 10327;
   else return 16828;
   }
 
@@ -39,19 +44,22 @@ function calcularValorFamiliar(sb_ant)
     return valorcfamiliar * cantcargas;
   }
 
-if(cfamiliares) calcularMontoTotal(calcularValorFamiliar(sb_ant), cantcargas);
+if(cfamiliares) calcularMontoTotal(calcularValorFamiliar(assignTramo(sb_ant)), cantcargas);
 
 
+function validateBool(booleanValue){
+  if(booleanValue) return "Si";
+  else return "No";
+}
 
-  /*if(sb_ant < 3264){
-    console.log("Al Trabajador " + nombre + " " + apellido + " le corresponde valor de familiar 0 por su renta del semestre anterior que es: " + sb_ant);
-  }
-  else if(sb_ant >= 3264 && sb_ant < 10327){
-    console.log("Al Trabajador " + nombre + " " + apellido + " le corresponde valor de familiar 3264 por su renta del semestre anterior que es: " + sb_ant);
-  }
-  else if(sb_ant >= 10327 && sb_ant < 16828){
-    console.log("Al Trabajador " + nombre + " " + apellido + " le corresponde valor de familiar 10327 por su renta del semestre anterior que es: " + sb_ant);
-  }
-  else{
-    console.log("Al Trabajador " + nombre + " " + apellido + " le corresponde valor de familiar 16828 por su renta del semestre anterior que es: " + sb_ant);
-  }}*/
+console.log("Nombre:" + nombre 
+  + "\nApellido:" + apellido 
+  + "\nCargas: "+ validateBool(cfamiliares) 
+  +"\nCantidad de Cargas familiares:" + cantcargas 
+  + "\nTramo: " + assignTramo(sb_ant) 
+  + "\nMonto por carga familiar: " + calcularValorFamiliar(assignTramo(sb_ant))
+  + "\nTotal Montos por cargas familiares: " + calcularMontoTotal(calcularValorFamiliar(assignTramo(sb_ant)), cantcargas)
+  + "\nSueldo del mes más cargas familiares: " + (sb_actual + calcularMontoTotal(calcularValorFamiliar(assignTramo(sb_ant)), cantcargas)));
+
+//console.log("Al Trabajador " + nombre + " " + apellido + " le corresponde valor de familiar " + calcularMontoTotal(calcularValorFamiliar(assignTramo(sb_ant)), cantcargas) + " por su renta del semestre anterior que es: " + sb_ant);
+
