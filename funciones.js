73aa6de61f00050b7980dec258685b4a4f6bc218
Charlie Cosmuffin <145036409+Cosmuff1n@users.noreply.github.com>
2026-08-30 21:24:14 -0400

    new changes
    removed redundant contacto.html file
    added formulario.html file
    added some bugfixes to funciones.js
    Modified estadisticas.html

--- funciones.js
+++ funciones.js
@@ -1,52 +1,31 @@
-/*function validateStrings(word){
-    if(word && word.trim()){
-        console.log("Valid");
-    }
-    else{
-        console.log("not valid");
-    }
-}
-
-function validatePosInt(numint){
-    if(numint>0){
-        console.log("Valid");
-    }
-    else{
-        console.log("not valid");
-    }
-}
-
-function validatePosDouble(numdou){
-    if(numdou>0){
-        console.log("valid");
-    }
-    else{
-        console.log("not valid");
-    }
-}*/
-
 function validarFormulario() {
 
-    let nombre = document.getElementById("nombre").value;
-    let correo = document.getElementById("correo").value;
-    let mensaje = document.getElementById("mensaje").value;
+    const nombre = document.getElementById("nombre").value.trim();
+    const correo = document.getElementById("correo").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
 
-    let camposFaltantes = "";
+    let camposFaltantes = [];
 
     if (nombre === "") {
-        camposFaltantes = camposFaltantes + "- Nombre\n";
+        camposFaltantes.push("nombre");
     }
     if (correo === "") {
-        camposFaltantes = camposFaltantes + "- Correo Electrónico\n";
+        camposFaltantes.push("correo");
     }
     if (mensaje === "") {
-        camposFaltantes = camposFaltantes + "- Detalle del Pedido\n";
+        camposFaltantes.push("mensaje");
     }
 
-    if (camposFaltantes !== "") {
-        alert("Por favor, completa los siguientes campos:\n" + camposFaltantes);
+    if (camposVacios.length > 0) {
+        alert("Por favor, completa los siguientes campos:\n" + camposFaltantes.join("\n"));
+        return false;
     } else {
         // Si no falta nada, todo está correcto
         alert("¡Pedido enviado con éxito!");
+        return true;
     }
 }
+
+document
+    .getElementById("formContacto")
+    .addEventListener("submit", validarFormulario);
