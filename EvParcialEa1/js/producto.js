cd33316e2ecd15689077aa894fe2805fb00eb789
bherrera01 <ben.herrerae@duocuc.cl>
2026-09-24 16:12:36 -0300

    avance cargarproductos

--- EvParcialEa1/js/producto.js
+++ EvParcialEa1/js/producto.js
@@ -1,4 +1,4 @@
-const listaProducto = [
+const listaProductos = [
     {id: 1, nombre: "Arena para gatos", precio: 10000, imagen: "img/arena_gatos.png"},
     {id: 2, nombre: "Comida para gatos", precio: 35000, imagen: "img/comida_gatos.png"},
     {id: 3, nombre: "Comida para perros", precio: 40000, imagen: "img/comida_perros.png"},
@@ -8,6 +8,8 @@ const listaProducto = [
     {id: 7, nombre: "Jaula para conejos", precio: 120000, imagen: "img/jaula_conejos.png"},
     {id: 8, nombre: "Comida para conejos", precio: 40000, imagen: "img/comida_conejo.png"}
 
-    
+];
 
-]
+    function cargarProductos() {
+        
+    }
