d07896d7c629f5c85c9840bc96a8d2b5bf58469e
bherrera01 <ben.herrerae@duocuc.cl>
2026-09-24 17:00:01 -0300

    pixeles de las fotos

--- EvParcialEa1/js/producto.js
+++ EvParcialEa1/js/producto.js
@@ -1,13 +1,13 @@
 //INDICE LISTA PRODUCTOS
 const listaProductos = [
-    { id: 1, nombre: "Arena para gatos", precio: 10000, imagen: "assets/arena_gatos.png" },
-    { id: 2, nombre: "Comida para gatos", precio: 35000, imagen: "assets/comida_gatos.png" },
-    { id: 3, nombre: "Comida para perros", precio: 40000, imagen: "assets/comida_perros.png" },
-    { id: 4, nombre: "Cama para perros", precio: 15000, imagen: "assets/cama_perros.png" },
-    { id: 5, nombre: "Pecera", precio: 50000, imagen: "assets/pecera_tortuga.png" },
-    { id: 6, nombre: "Calentador de agua para tortugas", precio: 15000, imagen: "assets/calentador_pecera.png" },
-    { id: 7, nombre: "Jaula para conejos", precio: 120000, imagen: "assets/jaula_conejos.png" },
-    { id: 8, nombre: "Comida para conejos", precio: 40000, imagen: "assets/comida_conejo.png" }
+    { id: 1, nombre: "Arena para gatos", precio: 10000, imagen: "assets/arena_gatos.png", px: 100 },
+    { id: 2, nombre: "Comida para gatos", precio: 35000, imagen: "assets/comida_gatos.png", px: 100 },
+    { id: 3, nombre: "Comida para perros", precio: 40000, imagen: "assets/comida_perros.png", px: 100 },
+    { id: 4, nombre: "Cama para perros", precio: 15000, imagen: "assets/cama_perros.png", px: 100 },
+    { id: 5, nombre: "Pecera", precio: 50000, imagen: "assets/pecera_tortuga.png", px: 100 },
+    { id: 6, nombre: "Calentador de agua para tortugas", precio: 15000, imagen: "assets/calentador_pecera.png", px: 100 },
+    { id: 7, nombre: "Jaula para conejos", precio: 120000, imagen: "assets/jaula_conejos.png", px: 100 },
+    { id: 8, nombre: "Comida para conejos", precio: 40000, imagen: "assets/comida_conejo.png", px: 100 }
 
 ];
 
