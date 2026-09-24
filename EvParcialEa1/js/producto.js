ebba69297c24fbcfd23f40e2de13e65edd66976e
bherrera01 <ben.herrerae@duocuc.cl>
2026-09-24 16:05:23 -0300

    productos

---
+++ EvParcialEa1/js/producto.js
@@ -0,0 +1,13 @@
+const listaProducto = [
+    {id: 1, nombre: "Arena para gatos", precio: 10000, imagen: "img/arena_gatos.png"},
+    {id: 2, nombre: "Comida para gatos", precio: 35000, imagen: "img/comida_gatos.png"},
+    {id: 3, nombre: "Comida para perros", precio: 40000, imagen: "img/comida_perros.png"},
+    {id: 4, nombre: "Cama para perros", precio: 15000, imagen: "img/cama_perros.png"},
+    {id: 5, nombre: "Pecera", precio: 50000, imagen: "img/pecera_tortuga.png"},
+    {id: 6, nombre: "Calentador de agua para tortugas", precio: 15000, imagen: "img/calentador_pecera.png"},
+    {id: 7, nombre: "Jaula para conejos", precio: 120000, imagen: "img/jaula_conejos.png"},
+    {id: 8, nombre: "Comida para conejos", precio: 40000, imagen: "img/comida_conejo.png"}
+
+    
+
+]
